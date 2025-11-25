1b381badb54ff665ad6df6163392dc239d5372ff
Rohit Kumar (IN45085) <rohit.10845085@ltimindtree.com>
2025-11-25 15:39:50 +0530

    adding customer ms

--- MICROSERVICES/LTI-SPRING-MICROSERVICE-FLIPFIT-NOTIFICATION-PRODUCER/src/main/java/com/lti/flipfit/service/GymFlipFitNotificationService.java
+++
@@ -1,7 +0,0 @@
-package com.lti.flipfit.service;
-
-import com.lti.flipfit.bean.GymFlipFitNotification;
-
-public interface GymFlipFitNotificationService {
-	public GymFlipFitNotification sendNotification(GymFlipFitNotification notification);
-}
