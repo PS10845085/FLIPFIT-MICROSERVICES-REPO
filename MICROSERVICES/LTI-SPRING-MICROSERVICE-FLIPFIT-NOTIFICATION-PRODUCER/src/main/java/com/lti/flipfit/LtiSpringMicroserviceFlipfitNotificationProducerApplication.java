c65767201ac968736e85ce2965ae4d7718721e85
Rohit Kumar (IN45085) <rohit.10845085@ltimindtree.com>
2025-11-25 15:47:56 +0530

    adding 3 ms

---
+++ MICROSERVICES/LTI-SPRING-MICROSERVICE-FLIPFIT-NOTIFICATION-PRODUCER/src/main/java/com/lti/flipfit/LtiSpringMicroserviceFlipfitNotificationProducerApplication.java
@@ -0,0 +1,13 @@
+package com.lti.flipfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LtiSpringMicroserviceFlipfitNotificationProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LtiSpringMicroserviceFlipfitNotificationProducerApplication.class, args);
+	}
+
+}
---
+++ MICROSERVICES/LTI-SPRING-MICROSERVICE-FLIPFIT-SCHEDULER-PRODUCER/src/main/java/com/lti/flipfit/LtiSpringMicroserviceFlipfitSchedulerProducerApplication.java
@@ -0,0 +1,13 @@
+package com.lti.flipfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LtiSpringMicroserviceFlipfitSchedulerProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LtiSpringMicroserviceFlipfitSchedulerProducerApplication.class, args);
+	}
+
+}
