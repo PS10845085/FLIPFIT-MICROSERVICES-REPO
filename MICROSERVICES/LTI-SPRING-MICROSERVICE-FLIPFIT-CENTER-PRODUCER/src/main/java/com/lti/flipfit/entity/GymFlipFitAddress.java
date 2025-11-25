7cd72172d8626586cfd272c48f24357997373e4d
Mallappa <mallappa.10844305@ltimindtree.com>
2025-11-25 16:15:15 +0530

    adding the entity class for center MS

---
+++ MICROSERVICES/LTI-SPRING-MICROSERVICE-FLIPFIT-CENTER-PRODUCER/src/main/java/com/lti/flipfit/entity/GymFlipFitAddress.java
@@ -0,0 +1,92 @@
+package com.lti.flipfit.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "gymaddress")
+public class GymFlipFitAddress {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(length = 100)
+	private String line1;
+
+	@Column(length = 100)
+	private String line2;
+
+	@Column(length = 50)
+	private String city;
+
+	@Column(length = 50)
+	private String state;
+
+	@Column(length = 10)
+	private String postalCode;
+
+	@Column(length = 50)
+	private String country;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getLine1() {
+		return line1;
+	}
+
+	public void setLine1(String line1) {
+		this.line1 = line1;
+	}
+
+	public String getLine2() {
+		return line2;
+	}
+
+	public void setLine2(String line2) {
+		this.line2 = line2;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+}
---
+++ MICROSERVICES/LTI-SPRING-MICROSERVICE-FLIPFIT-CENTER-PRODUCER/src/main/java/com/lti/flipfit/entity/GymFlipFitCenter.java
@@ -0,0 +1,107 @@
+package com.lti.flipfit.entity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "gymcenter")
+public class GymFlipFitCenter {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(length = 100, nullable = false)
+	private String name;
+
+	@Column(length = 100)
+	private String emailId;
+
+	@Column(length = 15)
+	private String phoneNo;
+
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "address_id", referencedColumnName = "id")
+	private GymFlipFitAddress address;
+
+	@Column(length = 20)
+	private String status;
+
+	@Column(nullable = true)
+	private Long adminId;
+
+	@Column(nullable = false)
+	private Long ownerId;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+	public String getPhoneNo() {
+		return phoneNo;
+	}
+
+	public void setPhoneNo(String phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+
+	public GymFlipFitAddress getAddress() {
+		return address;
+	}
+
+	public void setAddress(GymFlipFitAddress address) {
+		this.address = address;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public Long getAdminId() {
+		return adminId;
+	}
+
+	public void setAdminId(Long adminId) {
+		this.adminId = adminId;
+	}
+
+	public Long getOwnerId() {
+		return ownerId;
+	}
+
+	public void setOwnerId(Long ownerId) {
+		this.ownerId = ownerId;
+	}
+
+}
